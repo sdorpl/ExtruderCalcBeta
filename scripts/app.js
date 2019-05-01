c067c0b9e0f84d4827ce6f00478d95dde415ece3
sdorpl <macszymk@outlook.com>
2019-05-01 15:16:04 +0200

    version update

--- scripts/app.js
+++ scripts/app.js
@@ -26,7 +26,7 @@
     saveDialog: document.querySelector('.saveDialogContainer'),
     indexForm: document.getElementById('indexForm'),
     theme: showCookie("Theme"),
-    version: "0.6.46"
+    version: "0.6.47"
   };
 
 
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // ExtruderCalc
 
-const version = "0.6.46";
+const version = "0.6.47";
 const cacheName = `excalc-beta-${version}`;
 var filesToCache = [
   '/',
