6fa6b2553d21b4d13783b510a8aa76f2705a394c
sdorpl <macszymk@outlook.com>
2019-05-05 12:29:14 +0200

    upd

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ExtruderCalc
 
 
-const version = "0.6.66";
+const version = "0.6.67";
 const cacheName = `excalc-beta-${version}`;
 //document.getElementById('appver').innerHTML = `${version}-beta`;
 var filesToCache = [
