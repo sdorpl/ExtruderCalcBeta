b4bc22a008b6bf2a859232d1a63e41cdc505f2ab
sdorpl <macszymk@outlook.com>
2019-05-05 08:40:41 +0200

    change

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { apver } from './scripts/ver.js';
+import { apver } from './ver.js';
 
 alert(apver());
 
